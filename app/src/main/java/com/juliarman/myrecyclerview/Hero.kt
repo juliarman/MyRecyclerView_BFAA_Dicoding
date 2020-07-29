2bf28b43184b4188fc032b1de0b607e3440239ba
Juliarman Umar <juliarmanbone@gmail.com>
2020-07-30 07:30:22 +0800

    BFAA Dicoding MyRecyclerView

---
+++ app/src/main/java/com/juliarman/myrecyclerview/Hero.kt
@@ -0,0 +1,14 @@
+package com.juliarman.myrecyclerview
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class Hero (
+
+    var name: String,
+    var description: String,
+    var photo: String
+
+) : Parcelable
